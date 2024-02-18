420a76dab9feedc495185ee21f26dc84fc4475fc
vodebunm <vodebunm@student.42heilbronn.de>
2024-02-19 00:59:10 +0100

    get_next_line first update

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/18 01:00:46 by vodebunm          #+#    #+#             */
+/*   Updated: 2024/02/18 01:14:13 by vodebunm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+char	*get_next_line(int fd);
+
+#endif
